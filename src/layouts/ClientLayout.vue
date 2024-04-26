c527cda97a9c010ba8f2c44459e1a80756ef7e2f
Terence Cusi <tertercusi@gmail.com>
2024-04-26 14:57:33 +0800

    initial commit

---
+++ src/layouts/ClientLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-footer>
+      <q-tabs>
+        <q-route-tab
+          v-for="tab in tabList"
+          :key="tab.label"
+          :label="tab.label"
+          :icon="tab.icon"
+          :to="tab.to"
+        >
+        </q-route-tab>
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+const tabList = [
+  {
+    label: "Home",
+    icon: "home",
+    to: "/",
+  },
+];
+</script>
