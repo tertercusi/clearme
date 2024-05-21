cce91b56ddde0a29d21b86a6405bf5a372666f4c
Terence Cusi <tertercusi@gmail.com>
2024-05-21 21:18:55 +0800

    add login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -7,9 +7,9 @@
       <q-card-section class="column q-gutter-md">
         <span class="text-h4 text-center">Login</span>
 
-        <q-input label="Email"></q-input>
-        <q-input label="Password" type="password"></q-input>
-        <q-btn color="primary" label="Sign In"></q-btn>
+        <q-input label="Email" v-model="email"></q-input>
+        <q-input v-model="password" label="Password" type="password"></q-input>
+        <q-btn color="primary" label="Sign In" @click="login"></q-btn>
       </q-card-section>
     </q-card>
   </q-page>
@@ -17,4 +17,37 @@
 
 <script setup>
 import LogoImage from "components/LogoImage.vue";
+import { ref } from "vue";
+import { useFirebaseAuth, useFirestore } from "vuefire";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { getDoc, doc } from "firebase/firestore";
+import { useRouter } from "vue-router";
+
+const auth = useFirebaseAuth();
+const db = useFirestore();
+const router = useRouter();
+
+const email = ref("");
+const password = password("");
+
+async function login() {
+  try {
+    const user = await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+
+    const userDoc = await getDoc(doc(db, "users", user.user.uid));
+    const isStudent = userDoc.get("isStudent");
+
+    if (isStudent) {
+      router.push("/");
+    } else {
+      router.push("/admin");
+    }
+  } catch (e) {
+    alert(e.message);
+  }
+}
 </script>
