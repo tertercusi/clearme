200924f3d74cea48da4ed1dda7455627c97f2a42
Terence Cusi <tertercusi@gmail.com>
2024-05-22 12:27:11 +0800

    STABLE 1

--- quasar.config.js
+++ quasar.config.js
@@ -18,11 +18,7 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: [
-      "firebase",
-      "vuefire",
-      // "authGuard"
-    ],
+    boot: ["firebase", "vuefire", "authGuard"],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ["app.scss"],
